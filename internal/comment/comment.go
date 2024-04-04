3eadba625cc1a32dbe54dd1d6a0c950649566ec5
karokojnr <karoko.jnr99@gmail.com>
2024-04-04 22:07:26 +0300

    Added initial error handling for comment service

--- internal/comment/comment.go
+++ internal/comment/comment.go
@@ -2,7 +2,13 @@ package comment
 
 import (
 	"context"
+	"errors"
 	"fmt"
+)
+
+var (
+	ErrFetchingComment = errors.New("failed to fetch comment by id")
+	ErrNotImplemented  = errors.New("not implemented")
 )
 
 // Comment - Represents the comment structure
@@ -13,7 +19,8 @@ type Comment struct {
 	Author string
 }
 
-// Store -
+// Store - this interface defines all the methods
+// that the service needs to operate
 type Store interface {
 	GetComment(context.Context, string) (Comment, error)
 }
@@ -37,7 +44,19 @@ func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
 	cmt, err := s.Store.GetComment(ctx, id)
 	if err != nil {
 		fmt.Println(err)
-		return Comment{}, err
+		return Comment{}, ErrFetchingComment
 	}
 	return cmt, nil
+}
+
+func (s *Service) UpdateComment(ctx context.Context, cmt Comment) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) DeleteComment(ctx context.Context, id string) error {
+	return ErrNotImplemented
+}
+
+func (s *Service) CreateComment(ctx context.Context, cmt Comment) (Comment, error) {
+	return Comment{}, ErrNotImplemented
 }
