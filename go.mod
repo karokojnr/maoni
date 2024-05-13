16b27a84be3988d4bc64d4434c577c556fb86792
karokojnr <karoko.jnr99@gmail.com>
2024-05-13 15:19:59 +0300

    Add integration test for comment

--- go.mod
+++ go.mod
@@ -7,17 +7,20 @@ require (
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/lib/pq v1.10.9
 	github.com/sirupsen/logrus v1.9.3
+	github.com/stretchr/testify v1.9.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.3 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
 	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
 
 require (
---
+++ internal/db/comment_test.go
@@ -0,0 +1,49 @@
+//go:build integration
+
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/karokojnr/maoni/internal/comment"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCommentDatabase(t *testing.T) {
+	t.Run("test create comment", func(t *testing.T) {
+		db, err := NewDatabase()
+		assert.NoError(t, err)
+
+		cmt, err := db.PostComment(context.Background(), comment.Comment{
+			Slug:   "slug",
+			Author: "author",
+			Body:   "body",
+		})
+		assert.NoError(t, err)
+
+		newCmt, err := db.GetComment(context.Background(), cmt.ID)
+		assert.NoError(t, err)
+
+		assert.Equal(t, "slug", newCmt.Slug)
+
+	})
+
+	t.Run("test delete comment", func(t *testing.T) {
+		db, err := NewDatabase()
+		assert.NoError(t, err)
+
+		cmt, err := db.PostComment(context.Background(), comment.Comment{
+			Slug:   "slug1",
+			Author: "author1",
+			Body:   "body1",
+		})
+		assert.NoError(t, err)
+
+		err = db.DeleteComment(context.Background(), cmt.ID)
+		assert.NoError(t, err)
+
+		_, err = db.GetComment(context.Background(), cmt.ID)
+		assert.Error(t, err)
+	})
+}
